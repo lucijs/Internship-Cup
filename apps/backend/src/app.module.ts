6f668390776d61440fa528abdc85e8a10122779d
lucijs <fradelic.lucija@gmail.com>
2024-04-28 22:19:29 +0200

    added features to simultaneously launch backend and frontend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'frontend', 'dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('backend');
+  await app.listen(3001);
 }
 bootstrap();
