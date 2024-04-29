ea28b12831576bd8fc90563a14def18577c2c857
SimeJadrijev <sime.jadrijev@gmail.com>
2024-04-30 01:14:52 +0200

    defined basic endpoints for quiz questions

---
+++ apps/backend/src/quiz_questions/quiz_questions.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { QuizQuestionsService } from './quiz_questions.service';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { QuizQuestionEntity } from './entities/quiz_question.entity';
+
+@ApiTags('Quiz questions')
+@Controller('quiz-questions')
+export class QuizQuestionsController {
+  constructor(private readonly quizQuestionsService: QuizQuestionsService) {}
+
+  @Get()
+  @ApiCreatedResponse({ type: QuizQuestionEntity, isArray: true })
+  findAll() {
+    return this.quizQuestionsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiCreatedResponse({ type: QuizQuestionEntity })
+  findOne(@Param('id') id: string) {
+    return this.quizQuestionsService.findOne(+id);
+  }
+}
---
+++ apps/backend/src/quiz_questions/quiz_questions.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { QuizQuestionsService } from './quiz_questions.service';
+import { QuizQuestionsController } from './quiz_questions.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  controllers: [QuizQuestionsController],
+  providers: [QuizQuestionsService, PrismaModule],
+})
+export class QuizQuestionsModule {}
