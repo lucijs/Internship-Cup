e3cedd5bb2f0690be8e0f8fa86a6447e723a81de
SimeJadrijev <sime.jadrijev@gmail.com>
2024-04-29 23:53:47 +0200

    added swagger and exported prisma service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import {ServeStaticModule} from '@nestjs/serve-static';
 import {join} from 'path';
 import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'frontend', 'dist'),
     }),
+    CitiesModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
