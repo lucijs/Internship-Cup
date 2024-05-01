69b4f0aa0eff9f4f21e94d726a8a3f9985e111aa
lucijs <fradelic.lucija@gmail.com>
2024-05-01 04:39:27 +0200

    added dnd questions

--- apps/frontend/src/components/Questions/FillInQuestion/FillInQuestion.tsx
+++ apps/frontend/src/components/Questions/FillInQuestion/FillInQuestion.tsx
@@ -1,106 +1,116 @@
 import { useState } from 'react';
-import Draggable  from '../Draggable';
-import  Droppable  from '../Droppable';
-import {DndContext} from '@dnd-kit/core';
+import Draggable from '../Draggable';
+import Droppable from '../Droppable';
+import { DndContext } from '@dnd-kit/core';
 import { Over } from '@dnd-kit/core';
+import classes from './index.module.css'
 
 const FillInQuestion = () => {
-    const [parents, setParents] = useState<{ [key: string]: string | null }>({
-        droppable1: null,
-        droppable2: null,
-        droppable3: null,
-      });
-    
-      const [dragged, setDragged]=useState<{ [key: string]: string | null }>({
-        draggable1: null,
-        draggable2: null,
-        draggable3: null,
-      })
-    
-      const draggable = (id: string) => (
-        <Draggable id={id}>
-          {id}
-        </Draggable>
-      );
-    
-      const [drag, setDrag] = useState<String|null>(null);
-    
-      const getParentContent = (idDrop: string) => {
-        const id = parents[idDrop];
-        return id !== null ? draggable(id) : 'Drop here';
-      };
-    
-      const droppable = (idDrop: string) => (
-        <Droppable id={idDrop}>
-          {getParentContent(idDrop)}
-        </Droppable>
-      );
+  const text1 = "Prvi dio teksta";
+  const text2 = "Prvi dio teksta";
+  const text3 = "Prvi dio teksta";
+  const text4 = "Prvi dio teksta";
 
-      function handleDragStart(event: any) {
-        // Postavljanje drag elementa na ID povučenog elementa
-        setDrag(event.active.id);
+  // Collects where each item has been dropped
+  const [parents, setParents] = useState<{ [key: string]: string | null }>({
+    droppable1: null,
+    droppable2: null,
+    droppable3: null,
+  });
+
+  // Collects items that have been dragged
+  const [dragged, setDragged] = useState<{ [key: string]: string | null }>({
+    draggable1: null,
+    draggable2: null,
+    draggable3: null,
+  });
+
+  // Draggable items
+  const draggable = (id: string) => (
+    <Draggable id={id}>
+      {id}
+    </Draggable>
+  );
+
+  // Represents the currently dragged item
+  const [drag, setDrag] = useState<string | null>(null);
+
+  const getParentContent = (idDrop: string) => {
+    const id = parents[idDrop];
+    return id !== null ? draggable(id) : 'Drop here';
+  };
+
+  // Droppable items
+  const droppable = (idDrop: string) => (
+    <Droppable id={idDrop}>
+      {getParentContent(idDrop)}
+    </Droppable>
+  );
+
+  function handleDragStart(event: any) {
+    setDrag(event.active.id);
+  }
+
+  function handleDragEnd({ over }: { over: Over | null }) {
+    const draggedId = dragged[String(drag)];
+    if (over) {
+      const droppableId: string = String(over.id);
+      if (parents[droppableId] === null) {
+        setDragged((prev) => ({
+          ...prev,
+          [String(drag)]: droppableId,
+        }));
+        setParents((prevParents) => ({
+          ...prevParents,
+          [droppableId]: String(drag),
+          [String(draggedId)]: null,
+        }));
+      } else {
+        setDragged((prev) => ({
+          ...prev,
+          [String(parents[droppableId])]: null,
+          [String(drag)]: droppableId,
+        }));
+        setParents((prevParents) => ({
+          ...prevParents,
+          [droppableId]: String(drag),
+        }));
       }
-    
-      function handleDragEnd({ over }: { over: Over | null }) {
-        if (over) {
-          const droppableId: string = String(over.id);
-          const d = dragged[String(drag)]
-          if(parents[droppableId]===null){
-            setDragged((prev)=>{
-              return {
-                ...prev,
-                [String(drag)]: droppableId,
-              }
-            })
-            setParents((prevParents) => {
-              return {
-                ...prevParents,
-                [droppableId]: String(drag),
-                [String(d)]:null,
-              };
-            });
-          }
-          else{
-            setDragged((prev)=>{
-              return {
-                ...prev,
-                [String(parents[droppableId])]: null,
-                [String(drag)]: droppableId,
-              }
-            })
-            setParents((prevParents) => {
-              return {
-                ...prevParents,
-                [droppableId]: String(drag),
-              };
-            });
-          }
-        } else {
-          const d = dragged[String(drag)]
-          setDragged((prev)=>{
-            return {
-              ...prev,
-              [String(drag)]: null,
-            }
-          })
-          setParents((prevParents) => ({
-            ...prevParents,
-            [String(d)]: null,
-          }));
-        }
-        setDrag(null); 
+    } else {
+      setDragged((prev) => ({
+        ...prev,
+        [String(drag)]: null,
+      }));
+      setParents((prevParents) => ({
+        ...prevParents,
+        [String(draggedId)]: null,
+      }));
     }
+    setDrag(null);
+  }
 
-    return (<>
-    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
-      {dragged.draggable1===null? draggable('draggable1') : null}
-      {dragged.draggable2 ===null? draggable('draggable2') : null}
-      {dragged.draggable3 ===null? draggable('draggable3') : null}
-      {droppable('droppable1')}
-      {droppable('droppable2')}
-      {droppable('droppable3')}
-    </DndContext>
-    </>)
-}
+  return (
+    <>
+      <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+        <div className={classes.question}>
+            <div className={classes.text}>
+                {text1}
+                {droppable('droppable1')}
+                {text2}
+                {droppable('droppable2')}
+                {text3}
+                {droppable('droppable3')}
+                {text4}
+            </div>
+            <div className={classes.answer}>
+                {dragged.draggable1 === null ? draggable('draggable1') : null}
+                {dragged.draggable2 === null ? draggable('draggable2') : null}
+                {dragged.draggable3 === null ? draggable('draggable3') : null}
+            </div>
+        </div>
+      </DndContext>
+    </>
+  );
+};
 
 export default FillInQuestion;
