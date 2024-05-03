efba43498d38e7ab14da5b6c1e47071f5a16aa62
lucijs <fradelic.lucija@gmail.com>
2024-05-03 18:03:32 +0200

    displaying questions in quiz

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import FillInQuestion from "./components/Questions/DnDQuestion/FillInQuestion";
 import MatchingQuestion from "./components/Questions/DnDQuestion/MatchingQuestion/MatchingQuestion";
 import SliderQuestion from "./components/Questions/SliderQuestion";
-import Quiz from "./Pages/QuziPage";
+import Quiz from "./Pages/QuizPage";
 
 function App() { return (
     <>
--- apps/frontend/src/components/Questions/SliderQuestion/SliderQuestion.tsx
+++ apps/frontend/src/components/Questions/SliderQuestion/SliderQuestion.tsx
@@ -1,27 +1,15 @@
-import { Slider } from "@mui/material";
-import React, { useState } from "react";
-
-const SliderQuestion: React.FC = () => {
-  const [value, setValue] = useState(0);
-  const handleChange = (_event: Event, newValue: number | number[]) => {
-    setValue(newValue as number);
-  };
-  return (
-    <>
-      <Slider
-        aria-label="Small steps"
-        value={value}
-        onChange={handleChange}
-        defaultValue={0}
-        step={1}
-        marks
-        min={-10}
-        max={10}
-        valueLabelDisplay="auto"
-      />
-      <h1>{value}</h1>
-    </>
-  );
+const SliderQuestion = ({
+  question,
+  possibleAnswers,
+  correctAnswer1,
+  correctAnswer2,
+}: {
+  question: string;
+  possibleAnswers: string[];
+  correctAnswer1: string[];
+  correctAnswer2: string[];
+}) => {
+  return <>{question}</>;
 };
 
 export default SliderQuestion;
