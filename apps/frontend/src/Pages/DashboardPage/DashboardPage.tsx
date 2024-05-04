ee052df4b421e186a83d8309da3bd80a7ce688e6
SimeJadrijev <sime.jadrijev@gmail.com>
2024-05-04 13:11:26 +0200

    highlighted quizzes layout design

--- apps/frontend/src/Pages/DashboardPage/DashboardPage.tsx
+++ apps/frontend/src/Pages/DashboardPage/DashboardPage.tsx
@@ -1,4 +1,5 @@
 import DailyAdvice from "../../components/Dashboard/DailyAdvice";
+import HighlightedQuiz from "../../components/Dashboard/HighlightedQuiz";
 import PointsCard from "../../components/Dashboard/PointsCard";
 import StreakCard from "../../components/Dashboard/StreakCard";
 import classes from "./index.module.css";
@@ -8,14 +9,24 @@ const DashboardPage = () => {
     <>
       <div className={classes.dashboardPageWrapper}>
         <h2>Pozdrav, Marko! 👋</h2>
+
         <div className={classes.dashboardIntroSection}>
           <DailyAdvice />
+
           <div className={classes.streakPointsSection}>
             <StreakCard />
             <PointsCard />
           </div>
         </div>
+
         <h3>Posebno za tebe</h3>
+
+        <h4 className={classes.dashboardSubtitle}>ISTAKNUTI KVIZOVI</h4>
+
+        <div className={classes.highlightedQuizzesContainer}>
+          <HighlightedQuiz />
+          <HighlightedQuiz />
+        </div>
       </div>
     </>
   );
